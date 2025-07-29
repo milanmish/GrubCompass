4dd70b0056862628d9d905bc6807ed8646dab744
Milan Mishra <milanmishr20@gmail.com>
2025-07-29 17:39:20 -0500

    Update

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ I'm building a compass that will point me and my friends to a random restaurant 
 We always have trouble deciding on a place to eat, so I'm building a device that does the job for us. After randomly picking a restaurant within a certain radius from our location, the compass needle will point in the direction of the unknown location. We then need to follow the compass until we reach the restaurant.
 
 Hardware:
--  
-- 
-- 
+-  ESP32-DevKitC ([Datasheet](https://mm.digikey.com/Volume0/opasdata/d220001/medias/docus/754/ESP32-DevKitC_GSG_Ver1.4_2017.pdf))
+- Adafruit PA1010D GPS [Datasheet](https://cdn-learn.adafruit.com/downloads/pdf/adafruit-mini-gps-pa1010d-module.pdf)
+- Adafruit LIS3MDL Magnetometer ([Datasheet](https://mm.digikey.com/Volume0/opasdata/d220001/medias/docus/60/4479_Web.pdf))
+- Continuous servo motor
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,79 @@
 #include <Arduino.h>
+#include <ESP32Servo.h>
+#include <Wire.h>
+#include <Adafruit_LIS3MDL.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_GPS.h>
+//#include <WiFi.h>
+//#include <HTTPClient.h>
+//#include <ArduinoJson.h>
 
-// put function declarations here:
-int myFunction(int, int);
+HardwareSerial mySerial(1);
+Adafruit_GPS GPS(&mySerial);
+
+Adafruit_LIS3MDL lis3mdl = Adafruit_LIS3MDL();
+
+Servo myServo;
+
+const float threshold = 5.0;
+const float targetHeading = 0.0;
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+  Serial.begin(115200);
+  Wire.begin();
+
+  if (!lis3mdl.begin_I2C()) {
+    Serial.println("Failed to find LIS3MDL chip");
+    while (1);
+  }
+  Serial.println("LIS3MDL connected");
+
+  //maybe delete later
+  lis3mdl.setRange(LIS3MDL_RANGE_4_GAUSS);
+  lis3mdl.setDataRate(LIS3MDL_DATARATE_10_HZ);
+
+  mySerial.begin(9600, SERIAL_8N1, 16, 17);  // RX=16, TX=17 (change later)
+  GPS.begin(9600);
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
+  delay(1000);
+
+  myServo.attach(18); // pwm pin
+  myServo.write(90);
+
+
+  // raising canes coordinates yum yum
+  float targetLat = 44.948169;
+  float targetLon = -93.236016;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  while (GPS.available()) {
+    char c = GPS.read();
+
+    if (GPS.newNMEAreceived()) {
+      if (GPS.parse(GPS.lastNMEA())) {
+        if (GPS.fix) {
+          float currentLat = GPS.latitude;
+          float currentLon = GPS.longitude;
+        }
+      }
+    }
+  }
+
+  sensors_event_t event; 
+  lis3mdl.getEvent(&event);
+
+  float x = event.magnetic.x;
+  float y = event.magnetic.y;
+  float headingDeg = atan2(y, x) * 180 / PI;
+
+  if (headingDeg < 0) {
+  headingDeg += 360.0;
+  }
+
+  myServo.write(90 - headingDeg);
 }
